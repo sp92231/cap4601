8bca80bab2abe9f6820ef9fa5ac37502a85453ed
Shanie <shanieportal@gmail.com>
2024-03-15 23:41:53 -0400

    Added Header

---
+++ BayesNet Inference/main.py
@@ -0,0 +1,9 @@
+"""
+Name:Shanie Portal
+Date: 03/15/24
+Assignment: Module 8: Project - BayesNet Inference and Sampling
+Due Date: 03/17/24
+About this project:(write a small blurb about what this project's goals are/what tasks it accomplishes)
+Assumptions:(write any assumptions made here (ex: assumes correct user input))
+All work below was performed by (Your Name)
+"""
