216672b2b84de89695726cf9b841a5861066495a
Shanie <shanieportal@gmail.com>
2024-03-15 23:45:08 -0400

    Updated Header

--- BayesNet Inference/main.py
+++ BayesNet Inference/main.py
@@ -3,7 +3,7 @@ Name:Shanie Portal
 Date: 03/15/24
 Assignment: Module 8: Project - BayesNet Inference and Sampling
 Due Date: 03/17/24
-About this project:(write a small blurb about what this project's goals are/what tasks it accomplishes)
-Assumptions:(write any assumptions made here (ex: assumes correct user input))
-All work below was performed by (Your Name)
+About this project: This project implements a Bayesian network in Python for probabilistic inference and sampling.
+Assumptions: Valid user input for evidence and acyclicity of the Bayesian network.
+All work below was performed by: Shanie Portal
 """
