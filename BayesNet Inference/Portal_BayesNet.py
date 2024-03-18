b348b340441467173248d99434144fb2ff9c5aec
Shanie <shanieportal@gmail.com>
2024-03-18 07:13:59 -0400

    Heading, Virtual Env, & Interpreter Setup

---
+++ BayesNet Inference/Portal_BayesNet.py
@@ -0,0 +1,9 @@
+"""
+Name: Shanie Portal
+Date: 03/15/24
+Assignment: Module 8: Project - BayesNet Inference and Sampling
+Due Date: 03/17/24
+About this project: BayesNet Inference and Sampling Probability Analysis of the Star Wars Survey.
+Assumptions: N/A
+All work below was performed by Shanie Portal
+"""
