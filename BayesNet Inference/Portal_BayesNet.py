2b14ea431e9260d9b012c2e97591d66dddae9654
Shanie <shanieportal@gmail.com>
2024-03-19 04:16:57 -0400

    Open StarWars.xlsx

--- BayesNet Inference/Portal_BayesNet.py
+++ BayesNet Inference/Portal_BayesNet.py
@@ -7,3 +7,16 @@ About this project: BayesNet Inference and Sampling Probability Analysis of the 
 Assumptions: N/A
 All work below was performed by Shanie Portal
 """
+
+from BayesNet import *
+import random
+import pandas as pd
+import numpy as np
+
+random.seed("aima-python")
+
+T, F = True, False
+
+file = r"StarWars.xlsx"
+dff = pd.read_excel(file)
+
