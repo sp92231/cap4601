6a2a1301f3edea4234a12b53c5f5baf61251c888
Shanie <shanieportal@gmail.com>
2024-03-26 06:27:05 -0400

    Initial Commit

---
+++ HMM/Portal_HMM.py
@@ -0,0 +1,21 @@
+"""
+Name: Shanie Portal
+Date: 03/24/2024
+Assignment: Module 9: Project - MM and HMM
+Due Date: 03/24/2024
+About this project: HMM and MM around a specific dataset
+Assumptions:NA
+All work below was performed by Shanie Portal. Heavily followed Dr. Works' videos.
+"""
+import random
+import pandas as pd
+import numpy as np
+from MarkovChain import *
+from MChainDraw import *
+from statistics import mean
+from probability import *
+from utils import rounder
+
+random.seed("aima-python")
+file = r"PRSA_Data_Tiantan_20130301-20170228.csv"
+df = pd.read_csv(file)
