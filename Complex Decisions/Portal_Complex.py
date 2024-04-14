b78c000900fac7dd5985a566ab0d482ec0b56f1a
Shanie <shanieportal@gmail.com>
2024-04-13 23:34:55 -0400

    Set up

---
+++ Complex Decisions/Portal_Complex.py
@@ -0,0 +1,19 @@
+"""
+Name: Shanie Portal
+Date: 04/08/2024
+Assignment: Module 11: Project - MDP
+Due Date: 04/07/2024
+About this project: Salmon fishing this year, calculated through MDP, Value, and Policy Iteration.
+Assumptions:NA
+All work below was performed by Shanie Portal. Closely followed Dr. Works' Videos. """
+
+import mdptoolbox
+import numpy as np
+
+##########################
+# The number of states
+STATES = 4
+# The number of actions
+ACTIONS = 2
+ACTION_FISH = 0
+ACTION_NULL = 1
