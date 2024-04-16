3aad085119df9e7a11500e237aaae0c9f0bc6e03
Shanie <shanieportal@gmail.com>
2024-04-15 23:58:20 -0400

    Import setup

--- Measuring Acuracy/Portal_Acuracy.py
+++ Measuring Acuracy/Portal_Acuracy.py
@@ -6,3 +6,11 @@ Due Date: 04/14/2024
 About this project: Measure accuracy on depth/training set size and create a decision tree.
 Assumptions:NA
 All work below was performed by Shanie Portal and Dr. Works"""
+
+from sklearn import tree #For our Decision Tree
+import pandas as pd # For our DataFrame
+import pydotplus # To create our Decision Tree Graph
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
