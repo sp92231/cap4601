eecf1402dc56ccfdf40eaf56e87366858ed6ca51
Shanie <shanieportal@gmail.com>
2024-04-15 23:57:51 -0400

    Initial Commit

---
+++ Measuring Acuracy/Portal_Acuracy.py
@@ -0,0 +1,8 @@
+"""
+Name: Shanie Portal
+Date: 04/13/2024
+Assignment: Module 12: Project -Learning Decision Tree
+Due Date: 04/14/2024
+About this project: Measure accuracy on depth/training set size and create a decision tree.
+Assumptions:NA
+All work below was performed by Shanie Portal and Dr. Works"""
